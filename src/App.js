17ebebd326f4bef79e31a248b33e9db93b6ab8f9
Francisco López García Mayorga <javier6282@hotmail.com>
2022-10-15 23:54:11 -0300

    [UPDTA] Card, Search and Pagination components are created.

--- src/App.js
+++ src/App.js
@@ -3,29 +3,49 @@ import "bootstrap/dist/js/bootstrap";
 import "./App.css";
 import React, { useState, useEffect } from "react";
 
-import logo from "./logo.svg";
-
 // Componentes
 
 import Card from "./components/Card/Card";
-import Filter from "./components/Filter/Filter";
-import Footer from "./components/Footer/Footer";
-import Navbar from "./components/Navbar/Navbar";
+// import Filter from "./components/Filter/Filter";
+// import Footer from "./components/Footer/Footer";
+// import Navbar from "./components/Navbar/Navbar";
 import Pagination from "./components/Pagination/Pagination";
 import Search from "./components/Search/Search";
 
 function App() {
+  let [fetchedData, updateFetchedData] = useState([]);
+  let { data } = fetchedData;
+  let [pageNumber, updatePageNumber] = useState(1);
+  let [search, setSearch] = useState("");
+
+  let api = `https://yts.mx/api/v2/list_movies.json?page=${pageNumber}&query_term=${search}`;
+
+  useEffect(() => {
+    (async function () {
+      let response = await fetch(api).then((res) => res.json());
+      updateFetchedData(response);
+    })();
+  }, [api]);
+
   return (
     <div className="App">
-      <h1 className="text-center mb-3">Characters</h1>
+      <h1 className="text-center mb-3">Películas</h1>
+      <Search setSearch={setSearch} updatePageNumber={updatePageNumber} />
       <div className="container">
         <div className="row">
           Filter component will be placed here
           <div className="col-lg-8 col-12">
-            <div className="row">Card component will be placed here</div>
+            <div className="row">
+              <Card results={data.movies} />
+            </div>
           </div>
         </div>
       </div>
+      <Pagination
+        info={data}
+        pageNumber={pageNumber}
+        updatePageNumber={updatePageNumber}
+      />
     </div>
   );
 }
---
+++ src/components/Card/Card.js
@@ -0,0 +1,49 @@
+import React from "react";
+import styles from "./Card.module.scss";
+
+const Card = ({ results }) => {
+  let display;
+  if (results) {
+    display = results.map((x) => {
+      let { id, medium_cover_image, title, year, language } = x;
+
+      return (
+        <div
+          key={id}
+          className="col-lg-4 col-md-6 col-sm-6 col-12 mb-4 position-relative text-dark"
+        >
+          <div
+            className={`${styles.card} d-flex flex-column justify-content-center`}
+          >
+            <img
+              className={`${styles.img} img-fluid`}
+              src={medium_cover_image}
+              alt={title}
+            />
+          </div>
+          <div className={`${styles.content}`}>
+            <div className="fs-6 fw-bold">{title}</div>
+            <div className="fs-6">{year}</div>
+          </div>
+          {(() => {
+            if (language) {
+              return (
+                <div
+                  className={`${styles.badge} position-absolute badge bg-secondary`}
+                >
+                  {language.toUpperCase()}
+                </div>
+              );
+            }
+          })()}
+        </div>
+      );
+    });
+  } else {
+    display = "No Characters Found :/";
+  }
+
+  return <>{display}</>;
+};
+
+export default Card;
